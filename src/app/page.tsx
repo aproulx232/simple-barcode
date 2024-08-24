62926bc84f3eba939be2d4b59ec90e7dfcbd29d5
Allen Proulx <allen.proulx232@gmail.com>
2024-08-24 11:45:04 -0400

    Submit form to update barcode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,57 @@
-'use client'
-import styles from './page.module.css'
-import Script from 'next/script'
+"use client";
+import styled from "styled-components";
 
 export default function Home() {
+
+
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const formJson = Object.fromEntries(formData.entries());
+    JsBarcode("#barcode", formJson["barcodeInput"].toString());
+    console.log(formJson);
+  }
+
   return (
-    <main className={styles.main}>
-      <Script src="https://cdn.jsdelivr.net/npm/jsbarcode@3.11.6/dist/JsBarcode.all.min.js"
-        id='bar-code'
-        onReady={() => {
-          window.JsBarcode(".barcode").init();
-          window.JsBarcode("#barcode", "Hi world!");
-        }} />
-      <div className={styles.description}>
+    <Main>
+      <script src="https://cdn.jsdelivr.net/npm/jsbarcode@3.11.6/dist/JsBarcode.all.min.js" />
+
+      <form onSubmit={handleSubmit}>
+        <InputContainer>
+          <input name="barcodeInput" placeholder="Enter Barcode Value" />
+          <button type="submit">Submit form</button>
+        </InputContainer>
+      </form>
+
+      <BarcodeContainer>
         <svg id="barcode"></svg>
-        <svg className="barcode"
-          jsbarcode-format="upc"
-          jsbarcode-value="123456789012"
-          jsbarcode-textmargin="0"
-          jsbarcode-fontoptions="bold">
-        </svg>
-      </div>
-    </main>
-  )
+      </BarcodeContainer>
+    </Main>
+  );
 }
 
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 6rem;
+  min-height: 100vh;
+`;
+
+const BarcodeContainer = styled.div`
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const InputContainer = styled.div`
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
